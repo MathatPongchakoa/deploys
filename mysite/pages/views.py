f40461453373abe84b534f9b982649ebfbfe8fe2
mathatpongchakoa <mathat.po.2546@gmail.com>
2025-08-28 23:24:41 +0700

    minipro

---
+++ mysite/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def page1(request):
+    return render(request, "pages/page1.html")
+
+def page2(request):
+    return render(request, "pages/page2.html")
+
+def page3(request):
+    return render(request, "pages/page3.html")
